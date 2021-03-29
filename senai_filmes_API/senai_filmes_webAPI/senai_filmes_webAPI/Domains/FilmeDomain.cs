cfb197fd3570fc7b88831e515f98dae9ea5465cc
bren-lari <brenlaarisantos3@gmail.com>
2021-03-29 19:21:20 -0300

    Pasta do VS community adicionada

---
+++ senai_filmes_API/senai_filmes_webAPI/senai_filmes_webAPI/Domains/FilmeDomain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webAPI.Domains
+{
+    public class FilmeDomain
+    {
+        public int idFIlme { get; set; }
+
+        public string titulo { get; set; }
+
+        public int idGenero { get; set; }
+    }
+
+}
