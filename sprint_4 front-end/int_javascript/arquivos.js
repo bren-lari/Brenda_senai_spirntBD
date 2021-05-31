2df2f9067b6e446ca2cc4a9dbb5abe670a9fed59
bren-lari <brenlaarisantos3@gmail.com>
2021-05-31 17:26:32 -0300

    pasta js adicionada

---
+++ sprint_4 front-end/int_javascript/arquivos.js
@@ -0,0 +1,74 @@
+// comando para aparecer algo no console
+// o 10 em c# seria uma string, mas no js 
+// pode ser utilizado com int tb
+
+console.log("10" / 2);
+console.log("10" + 2);
+console.log("5" * 10);
+
+// quando vamos utilizar numeros,
+// não podemos colocar vírgula, senão será entendido que é uma soma de texto
+console.log("10,2" / 2);
+// quando vamos utilizar numeros, precisamos colocar 
+// um ponto
+console.log("10.2" / 2);
+
+console.log(0.1 + 0.7);
+
+// ---------------------------
+// variável
+
+// quem define o que e int, string, array é o próprio js
+var meuNome = "Brenda Larissa";
+var idade = 17;
+
+// ---------------------------
+// const
+
+const nota = 0;
+const profissão = "DEV";
+const naodefinida;
+
+// ------------------------
+// let
+
+// ele não tão solto quanto o var
+// na maioria das vezes deve ser usado
+
+let nome = "Brenda Larissa";
+let idade = 17;
+
+// ---------------
+// hoisting (var)
+// declararmos um console antes de declarar uma variável
+console.log(banana);
+var banana = "Nanica";
+console.log(banana);
+
+// hoisting (let)
+// nao roda pois funcionaria com uma chave 
+console.log(banana);
+ 
+let banana = "Nanica";
+console.log(banana);
+
+// -------------
+// comparadores lógicos
+
+// = (atribuição/var)
+// == (igualdade/ comparação)
+// === (desigualdade estrita/ totalmente igual)
+// && (and)
+// 
+// ! (not)
+
+ 
+
+
+
+
+
+
+
+ 
+
